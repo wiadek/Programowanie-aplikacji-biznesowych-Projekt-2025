7104e2eef1e7de74f97b77c95d69ff4b49f49421
wiadek <155567894+wiadek@users.noreply.github.com>
2025-01-25 23:52:01 +0100

    test1

---
+++ src/main/java/com/example/roombooking/Projekt2025MsMkApplication.java
@@ -0,0 +1,13 @@
+package com.example.roombooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projekt2025MsMkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Projekt2025MsMkApplication.class, args);
+    }
+
+}
