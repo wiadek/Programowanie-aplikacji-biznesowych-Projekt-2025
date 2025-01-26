e1e5b1f966b07af5ac2ef3cd272396c302191f20
mickuj3 <242485@stud.usz.edu.pl>
2025-01-26 14:39:59 +0100

    Revert "RoomBookingService"
    This reverts commit ab8329551846a7bc5a559f98e541596bf8af8a8e.

--- src/main/java/com/example/roombooking/service/RoomBookingService.java
+++ src/main/java/com/example/roombooking/service/RoomBookingService.java
@@ -1,17 +1,4 @@
 package com.example.roombooking.service;
 
-import com.example.roombooking.model.RoomBooking;
-import com.example.roombooking.repository.RoomBookingRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-@Service
 public class RoomBookingService {
-
-    @Autowired
-    private RoomBookingRepository roomBookingRepository;
-
-    public void save(RoomBooking roomBooking){
-        roomBoookingRepository.save(roomBooking);
-    }
 }
