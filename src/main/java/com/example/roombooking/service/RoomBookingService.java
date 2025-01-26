fc593c09c6da9bd86d8b412ce982d7b77bb8581a
mickuj3 <242485@stud.usz.edu.pl>
2025-01-26 14:47:18 +0100

    RoomBookingService poprawa

--- src/main/java/com/example/roombooking/service/RoomBookingService.java
+++ src/main/java/com/example/roombooking/service/RoomBookingService.java
@@ -1,4 +1,19 @@
 package com.example.roombooking.service;
 
+import com.example.roombooking.model.RoomBooking;
+import com.example.roombooking.repository.RoomBookingRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class RoomBookingService {
+
+    @Autowired
+    private RoomBookingRepository roomBookingRepository;
+
+    public void save(RoomBooking roomBooking) {
+        roomBookingRepository.save(roomBooking);
+    }
 }
+
+
